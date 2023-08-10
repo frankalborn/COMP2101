c99e350f7071c7bce344695fab2bf3fdc523d59c
frankalborn <133273004+frankalborn@users.noreply.github.com>
2023-08-10 16:49:07 -0400

    Add files via upload

---
+++ TEST2.psm1
@@ -0,0 +1,38 @@
+﻿#LAB 1
+
+"Hello World"
+
+#LAB 2
+
+function welcome {
+    "Welcome to PowerShell!"
+}
+welcome
+
+
+function get-cpuinfo {
+    Get-CimInstance -ClassName Win32_Processor |
+    Select-Object Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCores | select -last 1
+}
+get-cpuinfo
+
+
+
+
+function get-mydisks {"                             Disk Info"
+    Get-WmiObject -Class Win32_DiskDrive | Select-Object Manufacturer, Model, FirmwareRevision, Size, SerialNumber
+    
+}
+get-mydisks
+
+
+#LAB 3
+
+function networkReport {                             
+    get-ciminstance win32_networkadapterconfiguration | where-object ipenabled | select-object Description, IPaddress, DNSServerSearchOrder, DNSDomainSuffixSearchOrder, Index, IPsubnet | Format-table -Autosize
+    
+}
+networkReport
+
+get-ciminstance win32_networkadapterconfiguration | where-object ipenabled | select-object Description, IPaddress, DNSServerSearchOrder, DNSDomainSuffixSearchOrder, Index, IPsubnet | Format-table -Autosize
+
