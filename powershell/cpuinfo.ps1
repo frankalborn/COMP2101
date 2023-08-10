1042332f010365540eb0ffd22e731aa9f14e699b
frankalborn <133273004+frankalborn@users.noreply.github.com>
2023-08-10 17:00:51 -0400

    Add files via upload

---
+++ powershell/cpuinfo.ps1
@@ -0,0 +1,6 @@
+﻿
+function get-cpuinfo {
+    Get-CimInstance -ClassName Win32_Processor |
+    Select-Object Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCores | select -last 1 #my output is duplicated on my VM so I type -last 1 for only last output
+}
+get-cpuinfo
---
+++ powershell/get-mydisks.ps1
@@ -0,0 +1,5 @@
+﻿function get-mydisks {"                             Disk Info"
+    Get-WmiObject -Class Win32_DiskDrive | Select-Object Manufacturer, Model, FirmwareRevision, Size, SerialNumber
+    
+}
+get-mydisks
